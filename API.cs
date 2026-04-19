e77642d5eea56fdd34cafb083a92d9e4997c3c85
aoife <96482169+aoleee@users.noreply.github.com>
2026-04-19 15:49:58 +0100

    readded api

---
+++ API.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static gymTracker.BodyPart;
+
+namespace gymTracker
+{
+    internal class API
+    {
+        internal class Program
+        {
+            static async Task Main(string[] args)
+            {
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("https://gym-fit.p.rapidapi.com/v1/exercises/search?limit=50&offset=0&bodyPart=Chest&equipment=Barbell&type=Compound"),
+                    Headers =
+    {
+        { "x-rapidapi-key", "cbbefe63bcmshf31fcbe6324517bp1741a8jsnb2adc49bdcff" },
+        { "x-rapidapi-host", "gym-fit.p.rapidapi.com" },
+    },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+
+                    APIResponse apiResponse = JsonConvert.DeserializeObject<APIResponse>(body);
+                    List<Result> allExercises = apiResponse.results;
+
+                    foreach (Result exercise in allExercises)
+                    {
+                        Console.WriteLine(exercise.name);
+                    }
+
+                    Console.WriteLine(body);
+
+                }
+            }
+        }
+    }
+}
---
+++ BodyPart.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace gymTracker
+{
+    internal class BodyPart
+    {
+        public class Result
+        {
+            [JsonProperty("id")]
+            public string id { get; set; }
+
+            [JsonProperty("name")]
+            public string name { get; set; }
+
+            [JsonProperty("bodyPart")]
+            public string bodyPart { get; set; }
+
+            [JsonProperty("image")]
+            public string image { get; set; }
+        }
+
+        public class APIResponse
+        {
+            [JsonProperty("results")]
+            public List<Result> results { get; set; }
+
+            [JsonProperty("total")]
+            public int total { get; set; }
+
+            [JsonProperty("count")]
+            public int count { get; set; }
+        }
+    }
+}
